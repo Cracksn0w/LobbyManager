326307b00edd5f222c31b523873a6831f21b370d
Moritz Klein <moriklein@web.de>
2015-11-09 15:51:01 +0100

    First Commit!

---
+++ LobbyManager/src/com/cracksn0w/lobbymanager/LobbyManagerPlugin.java
@@ -0,0 +1,12 @@
+package com.cracksn0w.lobbymanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LobbyManagerPlugin extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		this.getLogger().info("Lobby Manager running! :");
+	}
+	
+}
